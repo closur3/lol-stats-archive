55ae1065cd4a2073432fa9dea142d3c3ee4812b7
hsux <72607204+closur3@users.noreply.github.com>
2026-03-03 01:10:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -53,8 +53,8 @@ const utils = {
         if (diffDays <= 1) return "hsl(215, 80%, 45%)";
         if (diffDays <= 3) return "hsl(215, 70%, 50%)";
         if (diffDays <= 7) return "hsl(215, 55%, 55%)";
-        if (diffDays <= 14) return "hsl(215, 25%, 55%)";
-        return "hsl(215, 15%, 70%)";
+        if (diffDays <= 14) return "hsl(215, 35%, 60%)";
+        return "hsl(215, 25%, 55%)";
     },
     
     parseDate: (str) => {
